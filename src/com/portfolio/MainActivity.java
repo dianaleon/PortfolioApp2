24ffd6ae574e6fa19dc82d23a366d51774358d31
dianaleon <dianileon@gmail.com>
2014-07-02 10:47:14 -0300

    aplicación nro2 arquitecto

---
+++ src/com/portfolio/MainActivity.java
@@ -0,0 +1,47 @@
+package com.portfolio;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.portfolio.activities.HomeActivity;
+import com.portfolio.activities.TextActivity;
+import com.portfolio.listener.IPortfolioListener;
+import com.portfolio.model.PortfolioModel;
+import com.portfolio.model.interfaces.IMenu;
+import com.portfolio.utils.Config;
+
+public class MainActivity extends Activity implements IPortfolioListener {
+
+	public String tittleApp = null;
+	public String subtittleApp = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Config.URL = "http://diana4love.com/AppsWorld/appsworld2";
+		PortfolioModel portfolioModel = PortfolioModel.getInstance(this);
+		portfolioModel.getPortfolio(this);
+	}
+
+	@Override
+	public void onPortfolioReady() {
+		PortfolioModel portfolioModel = PortfolioModel.getInstance(this);
+		IMenu menu = portfolioModel.getPorfolioMenu();
+		menu.getTitle();
+		tittleApp = menu.getTitle();
+		subtittleApp = menu.getSubtitle();
+		menu.getBackground();
+
+		Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+		startActivity(intent);
+		finish();
+	}
+
+	@Override
+	public void errorGetPortfolio() {
+		Intent intent = new Intent(MainActivity.this, TextActivity.class);
+		intent.putExtra("text", "");
+		startActivity(intent);
+	}
+}
